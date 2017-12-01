fcd408586462238c52775cd561ba2bc14f950d3e
William Wendell <william.wendell@deltagon.fi>
2017-12-01 11:32:27 +0200

    initial commit

---
+++ tmpl.js
@@ -0,0 +1,25 @@
+/*! Ez JS template engine | (c) 2016 William Wendell */
+var Template = (function(){
+	var Parser = function(){
+		$.extend(this, {
+			tmpl: (function(h){
+				// scrape first html comment content from each
+				// template container and remove it
+				$('.\\.tmpl').each(function(){
+					h[this.id] = $(this).contents().filter(function() {
+						return this.nodeType === 8;
+					})[0].nodeValue;
+					$(this).remove();
+				});
+				return h;
+			})({})
+		});
+		return $.proxy(function(t, data) {
+			if (!(''+this.tmpl[t]).length) { return; }
+			return this.tmpl[t].replace(/{[^{}]+}/g, function(key) {
+				return data[key.replace(/[{}]+/g, "")] || "";
+			})
+		},this);
+	};
+	return new Parser();
+})();
